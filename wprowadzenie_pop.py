d0e2c0c6aa55bbacd73b966ced2acd282e568300
Daniel <dsa@dsada.pl>
2020-03-06 16:38:11 +0100

    ,l;,l;

---
+++ wprowadzenie_pop.py
@@ -0,0 +1,18 @@
+a="Hello!"
+print(a)
+a=3
+print(a)
+a=3*1.0
+print(a) # sasa
+a,_b=3,6.5
+a="Hello %(a)d"
+print(a % {'a' : _b})
+_b+=3
+print(a % {'a' : _b})
+a=u"Helloł"
+print(a % {'a' : _b})
+lista = [3232,232,343, "sdas", 3.3]
+print(lista)
+dictionary = {'a':_b}
+print(dictionary)
+
